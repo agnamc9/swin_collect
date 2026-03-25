82699084770ae70906372f08eb6c3826fe9587fc
agnamcTechnocify <boris.agnaramon@technocify.fr>
2026-03-25 10:59:01 +0000

    integration logo sur recu

--- android/app/src/main/kotlin/com/softwin_ci/tax_collect/MainActivity.kt
+++ android/app/src/main/kotlin/com/softwin_ci/tax_collect/MainActivity.kt
@@ -3,6 +3,7 @@ package com.softwin_ci.tax_collect
 import android.os.Bundle
 import android.os.RemoteException
 import android.util.Log
+import android.graphics.BitmapFactory
 import androidx.annotation.NonNull
 import com.wisepos.smartpos.InitPosSdkListener
 import com.wisepos.smartpos.WisePosSdk
@@ -19,28 +20,29 @@ class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         wisePosSdk = WisePosSdk.getInstance()
-        wisePosSdk!!.initPosSdk(this, object : InitPosSdkListener {
-            override fun onInitPosSuccess() {
-                Log.d("sdkdemo", "initPosSdk: success!")
-            }
+        wisePosSdk!!.initPosSdk(
+                this,
+                object : InitPosSdkListener {
+                    override fun onInitPosSuccess() {
+                        Log.d("sdkdemo", "initPosSdk: success!")
+                    }
 
-            override fun onInitPosFail(i: Int) {
-                Log.d("sdkdemo", "initPosSdk: fail!")
-            }
-        })
+                    override fun onInitPosFail(i: Int) {
+                        Log.d("sdkdemo", "initPosSdk: fail!")
+                    }
+                }
+        )
     }
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            "androidChannel"
-        ).setMethodCallHandler { call, result ->
-            if (call.method == "wiseasyPrint") {
-                val data = call.arguments as String  // texte à imprimer
-                printReceipt(data)
-            }
-        }
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "androidChannel")
+                .setMethodCallHandler { call, result ->
+                    if (call.method == "wiseasyPrint") {
+                        val data = call.arguments as String // texte à imprimer
+                        printReceipt(data)
+                    }
+                }
     }
 
     private fun printReceipt(data: String) {
@@ -61,10 +63,10 @@ class MainActivity : FlutterActivity() {
             val map = mPrinter.getPrinterStatus()
 
             // 4. Ajouter le logo (depuis assets/)
-//            val inputStream = assets.open("logo-setbc-.png")
-//            val bitmap = BitmapFactory.decodeStream(inputStream)
-//            mPrinter.addPicture(PRINT_STYLE_CENTER, bitmap)
-//            inputStream.close()
+            val inputStream = assets.open("grand-zattry-2.png")
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            mPrinter.addPicture(PRINT_STYLE_CENTER, bitmap)
+            inputStream.close()
 
             // 5. Espacement après logo
             val spacingInfo = TextInfo()
@@ -85,7 +87,7 @@ class MainActivity : FlutterActivity() {
             val textInfo = TextInfo()
             textInfo.setAlign(PRINT_STYLE_LEFT)
             textInfo.setFontSize(22)
-            textInfo.setText(data)        // ← contenu envoyé depuis Flutter
+            textInfo.setText(data) // ← contenu envoyé depuis Flutter
             mPrinter.addSingleText(textInfo)
 
             mPrinter.addSingleText(spacingInfo)
@@ -95,19 +97,28 @@ class MainActivity : FlutterActivity() {
             textInfo.setText("Softwin © 2026\n")
             mPrinter.addSingleText(textInfo)
 
+            
+            val softwinBitmap = BitmapFactory.decodeStream(assets.open("softwin.png"))
+            mPrinter.addPicture(PRINT_STYLE_CENTER, softwinBitmap)
+            inputStream.close()
+
             // 8. Lancer l'impression
-            mPrinter.startPrinting(Bundle(), object : PrinterListener {
-                override fun onError(i: Int) { /* gérer l'erreur */
-                }
+            mPrinter.startPrinting(
+                    Bundle(),
+                    object : PrinterListener {
+                        override fun onError(i: Int) {
+                            /* gérer l'erreur */
+                        }
 
-                override fun onFinish() {
-                    mPrinter.feedPaper(24)  // avancer le papier
-                }
+                        override fun onFinish() {
+                            mPrinter.feedPaper(24) // avancer le papier
+                        }
 
-                override fun onReport(i: Int) { /* réservé */
-                }
-            })
-
+                        override fun onReport(i: Int) {
+                            /* réservé */
+                        }
+                    }
+            )
         } catch (e: RemoteException) {
             e.printStackTrace()
         }

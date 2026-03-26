56bda72997493af0dc4e145bd4fff7f734ecb4cb
agnamcTechnocify <boris.agnaramon@technocify.fr>
2026-03-26 15:18:27 +0000

    Mise a jour icone et affichage numero version

--- lib/src/presentation/signin/screens/signin_screen.dart
+++ lib/src/presentation/signin/screens/signin_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:tax_collect/src/presentation/presentation.dart';
 import 'package:tax_collect/src/widgets/dialogs.dart';
 
@@ -19,6 +20,7 @@ class _SigninScreenState extends ConsumerState<SigninScreen> {
 
   final _formKey = GlobalKey<FormState>();
 
+  String? appVersion;
   @override
   void initState() {
     _signinController = ref.read(signinControllerProvider);
@@ -27,6 +29,17 @@ class _SigninScreenState extends ConsumerState<SigninScreen> {
       _passwordController.text = "12345678Aa";
     }
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _getAppVersion();
+    });
+  }
+
+  void _getAppVersion() async {
+    PackageInfo.fromPlatform().then((packageInfo) {
+      setState(() {
+        appVersion = packageInfo.version;
+      });
+    });
   }
 
   @override
@@ -35,86 +48,109 @@ class _SigninScreenState extends ConsumerState<SigninScreen> {
       appBar: AppBar(title: Text("Connexion"), centerTitle: true),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  'Veuillez renseigner vos identifiants pour vous connecter',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16),
-                ),
-                Gap(16),
-                RichText(
-                  text: TextSpan(
-                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-                    text: "Identifiant",
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      TextSpan(
-                        text: " *",
-                        style: TextStyle(color: Colors.red),
+                      Text(
+                        'Veuillez renseigner vos identifiants pour vous connecter',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Gap(16),
+                      RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          text: "Identifiant",
+                          children: [
+                            TextSpan(
+                              text: " *",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Gap(8),
+                      TextFormField(
+                        controller: _usernameController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "L'identifiant est requis";
+                          }
+                          return null;
+                        },
+                      ),
+                      Gap(16),
+                      RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          text: "Mot de passe",
+                          children: [
+                            TextSpan(
+                              text: " *",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Gap(16),
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Le mot de passe";
+                          }
+                          return null;
+                        },
+                      ),
+                      Gap(16),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _submit();
+                          }
+                        },
+                        child: Text("Se connecter"),
                       ),
                     ],
                   ),
                 ),
-                Gap(8),
-                TextFormField(
-                  controller: _usernameController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "L'identifiant est requis";
-                    }
-                    return null;
-                  },
-                ),
-                Gap(16),
-                RichText(
-                  text: TextSpan(
-                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-                    text: "Mot de passe",
-                    children: [
-                      TextSpan(
-                        text: " *",
-                        style: TextStyle(color: Colors.red),
-                      ),
-                    ],
-                  ),
-                ),
-                Gap(16),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Le mot de passe";
-                    }
-                    return null;
-                  },
-                ),
-                Gap(16),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _submit();
-                    }
-                  },
-                  child: Text("Se connecter"),
-                ),
-              ],
+              ),
             ),
-          ),
+            Gap(4),
+            Text(
+              appVersion != null ? 'v$appVersion' : '',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
@@ -122,12 +158,19 @@ class _SigninScreenState extends ConsumerState<SigninScreen> {
 
   void _submit() async {
     showLoadingDialog(context);
-    var response = await _signinController.signin(_usernameController.text, _passwordController.text);
+    var response = await _signinController.signin(
+      _usernameController.text,
+      _passwordController.text,
+    );
     Navigator.pop(context);
     if (!response.success!) {
       showInfoDialog(context, message: response.message ?? '');
       return;
     }
-    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => HomeScreen()), (route) => false);
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()),
+      (route) => false,
+    );
   }
 }
--- lib/src/presentation/splash/screens/splash_screen.dart
+++ lib/src/presentation/splash/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:tax_collect/src/data/data.dart';
 import 'package:tax_collect/src/presentation/home/home.dart';
 import 'package:tax_collect/src/presentation/presentation.dart';
@@ -12,20 +13,53 @@ class SplashScreen extends ConsumerStatefulWidget {
 }
 
 class _SplashScreenState extends ConsumerState<SplashScreen> {
+  String? appVersion;
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 2), () {
-      bool hasSession = ref.read(userRepositoryProvider).getUser() != null;
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => hasSession ? HomeScreen() : SigninScreen()),
-      );
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _getAppVersion();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: CircularProgressIndicator(strokeWidth: 1)));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(child: Center(child: CircularProgressIndicator())),
+            Text(
+              appVersion != null ? 'v$appVersion' : '',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _getAppVersion() async {
+    PackageInfo.fromPlatform().then((packageInfo) {
+      setState(() {
+        appVersion = packageInfo.version;
+      });
+      runSplash();
+    });
+  }
+
+  void runSplash() {
+    Future.delayed(const Duration(seconds: 3), () {
+      bool hasSession = ref.read(userRepositoryProvider).getUser() != null;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => hasSession ? HomeScreen() : SigninScreen(),
+        ),
+      );
+    });
   }
 }
